bffc93f2ff0a615911796d3e75cfd7af01f10c10
wwwvl <wwwvlad36025585@mail.ru>
2020-05-27 19:30:51 +0400

    cto-to

--- app/src/main/java/com/example/eto_ono/Graf_activity.java
+++ app/src/main/java/com/example/eto_ono/Graf_activity.java
@@ -31,10 +31,13 @@ public class Graf_activity extends AppCompatActivity {
         setContentView(R.layout.activity_graf_activity);
 
         AnyChartView anyChartView = findViewById(R.id.any_chart_view);
-        anyChartView.setProgressBar(findViewById(R.id.progress_bar));
+
+        Bundle extras = getIntent().getExtras();
+        int[] results =extras.getIntArray("results");
+        int k = extras.getInt("mass");
 
         Cartesian3d area3d = AnyChart.area3d();
-        area3d.xAxis(0).labels().format("${%Value}");
+        area3d.xAxis(0).labels().format("{%Value}");
 
         area3d.animation(true);
 
@@ -48,41 +51,20 @@ public class Graf_activity extends AppCompatActivity {
         area3d.title().padding(0d, 0d, 20d, 0d);
 
         List<DataEntry> seriesData = new ArrayList<>();
-        seriesData.add(new CustomDataEntry("1986", 162, 42));
-        seriesData.add(new CustomDataEntry("1987", 134, 54));
-        seriesData.add(new CustomDataEntry("1988", 116, 26));
-        seriesData.add(new CustomDataEntry("1989", 122, 32));
-        seriesData.add(new CustomDataEntry("1990", 178, 68));
-        seriesData.add(new CustomDataEntry("1991", 144, 54));
-        seriesData.add(new CustomDataEntry("1992", 125, 35));
-        seriesData.add(new CustomDataEntry("1993", 176, 66));
-        seriesData.add(new CustomDataEntry("1994", 156, 80));
-        seriesData.add(new CustomDataEntry("1995", 195, 120));
-        seriesData.add(new CustomDataEntry("1996", 215, 115));
-        seriesData.add(new CustomDataEntry("1997", 176, 36));
-        seriesData.add(new CustomDataEntry("1998", 167, 47));
-        seriesData.add(new CustomDataEntry("1999", 142, 72));
-        seriesData.add(new CustomDataEntry("2000", 117, 37));
-        seriesData.add(new CustomDataEntry("2001", 113, 23));
-        seriesData.add(new CustomDataEntry("2002", 132, 30));
-        seriesData.add(new CustomDataEntry("2003", 146, 46));
-        seriesData.add(new CustomDataEntry("2004", 169, 59));
-        seriesData.add(new CustomDataEntry("2005", 184, 44));
+        for (int i  = 0; i<k; i++)
+        {
+            seriesData.add(new CustomDataEntry(i, results[i]));
+        }
 
         Set set = Set.instantiate();
         set.data(seriesData);
         Mapping series1Data = set.mapAs("{ x: 'x', value: 'value' }");
-        Mapping series2Data = set.mapAs("{ x: 'x', value: 'value2' }");
 
         Area3d series1 = area3d.area(series1Data);
-        series1.name("ACME Share Price");
+        series1.name("Ударов");
         series1.hovered().markers(false);
         series1.hatchFill("diagonal", "#000", 0.6d, 10d);
 
-        Area3d series2 = area3d.area(series2Data);
-        series2.name("The Competitor\\'s Share Price");
-        series2.hovered().markers(false);
-        series2.hatchFill(HatchFillType.DIAGONAL_BRICK, "#000", 0.6d, 10d);
 
         area3d.tooltip()
                 .position(Position.CENTER_TOP)
@@ -98,9 +80,8 @@ public class Graf_activity extends AppCompatActivity {
     }
 
     private class CustomDataEntry extends ValueDataEntry {
-        CustomDataEntry(String x, Number value, Number value2) {
+        CustomDataEntry(Integer x, Number value) {
             super(x, value);
-            setValue("value2", value2);
         }
     }
 }
--- app/src/main/java/com/example/eto_ono/Second_activity.java
+++ app/src/main/java/com/example/eto_ono/Second_activity.java
@@ -8,7 +8,9 @@ import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.math.BigDecimal;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -16,8 +18,9 @@ public class Second_activity extends AppCompatActivity {
     TextView view_heart;
     TextView view_temp;
     private Handler handler;
-
-
+    int k = 0;
+    int i = 0;
+    int [] results = new int [10];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,17 +32,28 @@ public class Second_activity extends AppCompatActivity {
         butgraf.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Second_activity.this, Graf_activity.class);
-                startActivity(intent);
+                if((k!=5)&(k<5))
+                {
+                    Toast.makeText(getApplicationContext(), "Не хватает записей для графика, ждите.", Toast.LENGTH_LONG).show();
+                }
+                else
+                {
+                    Intent intent = new Intent(Second_activity.this, Graf_activity.class);
+                    intent.putExtra("mass", k);
+                    intent.putExtra("results", results);
+                    startActivity(intent);
+                }
             }
         });
         view_heart.setText("0");
         view_temp.setText("0°C");
         startTimer();
 
-
-
-
+    }
+    protected void onPause() {
+        super.onPause();
+        i = 0;
+        k = 0;
     }
 
     private void setValue(TextView textView, String str){
@@ -55,15 +69,26 @@ public class Second_activity extends AppCompatActivity {
                         new Runnable() {
                             @Override
                             public void run() {
-                                int result_heart = 80 + (int) ((Math.random() * 61) - 20);
+                                int result_heart =(int) (Math.random() * 41 + 80);
                                 setValue(view_heart,Integer.toString(result_heart));
-                                double result_temp = 36 + (int) ((Math.random() * 39) - 35);
-                                String stringdouble = Double.toString(result_temp);
-                                setValue(view_temp,stringdouble + "°C");
+                                float result_temp =(float) (Math.random() * 3.5 + 36);
+                                view_temp.setText(roundUp(result_temp, 1)+"°C");
+                                writting(result_heart);
                             }
                         }
                 );
             }
-        }, 0, 3500);
+        }, 0, 1500);
+    }
+    public static BigDecimal roundUp(float value, int digits){
+        return new BigDecimal("" + value).setScale(digits, BigDecimal.ROUND_HALF_UP);
+    }
+    public void writting (int res) {
+        if (i <10)
+        {
+            results[i] = res;
+            k++;
+            i++;
+        }
     }
 }
